438311aff01128b1e45026d67d20536607539bf2
kb <ken.bayama@gmail.com>
2021-01-11 15:24:52 +0100

    changes on the exercice2 and 3 accordings to the unreliability of the statements given ahead. completing the tests and the alternative functions and the handlings of the variaous result in an integrated way

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='shared',
+      version='0.0.1',
+      description='A sample PySpark application',
+      url='',
+      author='MrPowers',
+      author_email='',
+      packages=['src/dependencies'],
+      zip_safe=False)
